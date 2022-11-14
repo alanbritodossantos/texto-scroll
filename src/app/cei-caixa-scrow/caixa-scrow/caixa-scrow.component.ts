86396c2039fa9f5ab3f3d7457f85d24b0e1372b9
Alan <alanbritodossantos2015@gmail.com>
2022-11-14 01:38:47 -0300

    foi colocado logica para subir e descer o scroll

--- src/app/cei-caixa-scrow/caixa-scrow/caixa-scrow.component.ts
+++ src/app/cei-caixa-scrow/caixa-scrow/caixa-scrow.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 
 @Component({
   selector: 'app-caixa-scrow',
   templateUrl: './caixa-scrow.component.html',
   styleUrls: ['./caixa-scrow.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CaixaScrowComponent implements OnInit {
+export class CaixaScrowComponent {
+  @ViewChild('scroll', { read: ElementRef })
+  public scroll!: ElementRef<any>;
 
-  items = Array.from({length: 100000}).map((_, i) => `Item #${i}`);
+  scrollUp = true;
+  scrollDown = true;
 
+  public onPreviousSearchPosition(): void {
+    this.scrollUp = true;
+    this.scrollDown = true;
 
-  constructor() { }
+    if (this.scroll.nativeElement.scrollTop <= 20) {
+      this.scrollUp = false;
+    }
 
-  ngOnInit(): void {
+    this.scroll.nativeElement.scrollTop -= 20;
+  }
+
+  public onNextSearchPosition(): void {
+    this.scrollDown = true;
+    this.scrollUp = true;
+
+    if (
+      this.scroll.nativeElement.scrollTop >=
+      this.scroll.nativeElement.offsetHeight - 20
+    ) {
+      this.scrollDown = false;
+    }
+
+    this.scroll.nativeElement.scrollTop += 20;
   }
 
 }
