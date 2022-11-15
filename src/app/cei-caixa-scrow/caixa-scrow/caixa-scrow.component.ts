846112a711ea81693cbf9bed3425cd5c52b8bb47
Alan <alanbritodossantos2015@gmail.com>
2022-11-15 18:45:01 -0300

    foi corrigido bug de scroll botão descer

--- src/app/cei-caixa-scrow/caixa-scrow/caixa-scrow.component.ts
+++ src/app/cei-caixa-scrow/caixa-scrow/caixa-scrow.component.ts
@@ -12,36 +12,49 @@ import {
   styleUrls: ['./caixa-scrow.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CaixaScrowComponent {
+export class CaixaScrowComponent implements OnInit{
   @ViewChild('scroll', { read: ElementRef })
   public scroll!: ElementRef<any>;
 
   scrollUp = true;
   scrollDown = true;
 
+  reduzTamanhoAlturaScrollBotaoDescer = 276; //reduz o tamanho do scroll
+  reduzTamanhoAlturaScrollBotaoSubir = 20;
+
+ngOnInit(): void {
+  //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+  //Add 'implements OnInit' to the class.
+
+
+  this.scrollUp = false;
+}
+
+//botão subir
   public onPreviousSearchPosition(): void {
     this.scrollUp = true;
     this.scrollDown = true;
 
-    if (this.scroll.nativeElement.scrollTop <= 20) {
+    if (this.scroll.nativeElement.scrollTop <= this.reduzTamanhoAlturaScrollBotaoSubir) {
       this.scrollUp = false;
+      console.log("botão subir desativado");
     }
 
     this.scroll.nativeElement.scrollTop -= 20;
   }
 
+  //botão descer
   public onNextSearchPosition(): void {
     this.scrollDown = true;
     this.scrollUp = true;
 
-    if (
-      this.scroll.nativeElement.scrollTop >=
-      this.scroll.nativeElement.offsetHeight - 20
-    ) {
+    if (this.scroll.nativeElement.scrollTop >= this.scroll.nativeElement.scrollHeight - this.reduzTamanhoAlturaScrollBotaoDescer) {
       this.scrollDown = false;
+      console.log("botão descer desativado");
+
     }
 
-    this.scroll.nativeElement.scrollTop += 20;
+    this.scroll.nativeElement.scrollTop += 40;
   }
 
 }
